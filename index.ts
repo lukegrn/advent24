604ab11e1a5836cef2c17338e742160f66fce27f
Luke Green <me@lukegreen.xyz>
2024-12-10 21:11:12 -0600

    day 3 done

--- index.ts
+++ index.ts
@@ -2,9 +2,9 @@ import { part1 as day1part1, part2 as day1part2 } from "./solutions/day1";
 import { part1 as day2part1, part2 as day2part2 } from "./solutions/day2";
 import { part1 as day3part1, part2 as day3part2 } from "./solutions/day3";
 
-console.log(day1part1());
-console.log(day1part2());
-console.log(day2part1());
-console.log(day2part2());
-console.log(day3part1());
-console.log(day3part2());
+console.log("Day 1, part 1: ", day1part1());
+console.log("Day 1, part 2: ", day1part2());
+console.log("Day 2, part 1: ", day2part1());
+console.log("Day 2, part 2: ", day2part2());
+console.log("Day 3, part 1: ", day3part1());
+console.log("Day 3, part 2: ", day3part2());
--- solutions/day3.ts
+++ solutions/day3.ts
@@ -1,7 +1,54 @@
+import fs from "fs";
+
+const readData = () => {
+  return fs.readFileSync("./data/day3.txt").toString();
+};
+
 export const part1 = () => {
-  return "p1";
+  const data = readData();
+
+  const mults = [...data.matchAll(/mul\(\d+,\d+\)/g)].map((arr) => arr[0]);
+
+  return mults.reduce((acc, mult) => {
+    return (
+      acc +
+      [...mult.matchAll(/\d+/g)]
+        .map((arr) => Number(arr[0]))
+        .reduce((acc, num) => acc * num)
+    );
+  }, 0);
 };
 
 export const part2 = () => {
-  return "p2";
+  const data = readData();
+
+  const mults = [...data.matchAll(/(mul\(\d+,\d+\)|don\'t\(\)|do\(\))/g)].map(
+    (arr) => arr[0],
+  );
+
+  return mults.reduce(
+    (acc, cmd) => {
+      if (cmd == "do()") {
+        return { total: acc.total, do: true };
+      }
+
+      if (cmd == "don't()") {
+        return { total: acc.total, do: false };
+      }
+
+      if (cmd.includes("mul") && acc.do) {
+        return {
+          total:
+            acc.total +
+            [...cmd.matchAll(/\d+/g)]
+              .map((arr) => Number(arr[0]))
+              .reduce((acc, num) => acc * num),
+          do: acc.do,
+        };
+      }
+
+      return acc;
+    },
+    { total: 0, do: true },
+  ).total;
 };
