dbee6bc9dbba84cf4d301b63ce994a853dafa395
Luke Green <me@lukegreen.xyz>
2024-12-17 13:27:48 -0600

    better index runner

--- index.ts
+++ index.ts
@@ -5,15 +5,50 @@ import { part1 as day4part1, part2 as day4part2 } from "./solutions/day4";
 import { part1 as day5part1, part2 as day5part2 } from "./solutions/day5";
 import { part1 as day6part1, part2 as day6part2 } from "./solutions/day6";
 
-console.log("Day 1, part 1: ", day1part1());
-console.log("Day 1, part 2: ", day1part2());
-console.log("Day 2, part 1: ", day2part1());
-console.log("Day 2, part 2: ", day2part2());
-console.log("Day 3, part 1: ", day3part1());
-console.log("Day 3, part 2: ", day3part2());
-console.log("Day 4, part 1: ", day4part1());
-console.log("Day 4, part 2: ", day4part2());
-console.log("Day 5, part 1: ", day5part1());
-console.log("Day 5, part 2: ", day5part2());
-console.log("Day 6, part 1: ", day6part1());
-console.log("Day 6, part 2: ", day6part2());
+interface Solution {
+  part1: () => void;
+  part2: () => void;
+}
+
+const solutions: Solution[] = [
+  {
+    part1: day1part1,
+    part2: day1part2,
+  },
+  {
+    part1: day2part1,
+    part2: day2part2,
+  },
+  {
+    part1: day3part1,
+    part2: day3part2,
+  },
+  {
+    part1: day4part1,
+    part2: day4part2,
+  },
+  {
+    part1: day5part1,
+    part2: day5part2,
+  },
+  {
+    part1: day6part1,
+    part2: day6part2,
+  },
+];
+
+if (process.argv.length >= 4) {
+  console.error("Usage: npm run start -- <day>");
+}
+
+if (process.argv.length == 2) {
+  solutions.forEach((solution, i) => {
+    console.log(`Day ${i + 1} part 1: `, solution.part1());
+    console.log(`Day ${i + 1} part 2: `, solution.part2());
+  });
+} else {
+  const day = Number(process.argv[2]);
+  const i = day - 1;
+  console.log(`Day ${day} part1: `, solutions[i].part1());
+  console.log(`Day ${day} part2: `, solutions[i].part2());
+}
--- solutions/day6.ts
+++ solutions/day6.ts
@@ -45,21 +45,13 @@ const findStartingPosition = (matrix: string[][]): Point => {
   return point;
 };
 
-export const part1 = () => {
-  const data = readData();
-
-  const matrix = data.split("\n").map((line) => line.split(""));
+const walkPath = (matrix: string[][]) => {
   const dimensions: Point = { x: matrix[0].length, y: matrix.length };
   const at = findStartingPosition(matrix);
   const traveledTo: Point[] = [];
   let curr = ORDER.find((dir) => dir.char == matrix[at.y][at.x]);
 
-  while (
-    at.x + curr.x >= 0 &&
-    at.x + curr.x < dimensions.x &&
-    at.y + curr.y >= 0 &&
-    at.y + curr.y < dimensions.y
-  ) {
+  while (at.x >= 0 && at.x < dimensions.x && at.y >= 0 && at.y < dimensions.y) {
     if (
       traveledTo.findIndex((point) => point.x == at.x && point.y == at.y) == -1
     ) {
@@ -77,7 +69,15 @@ export const part1 = () => {
     at.y += curr.y;
   }
 
-  return traveledTo.length + 1;
+  return traveledTo;
+};
+
+export const part1 = () => {
+  const data = readData();
+
+  const matrix = data.split("\n").map((line) => line.split(""));
+
+  return walkPath(matrix).length;
 };
 
 export const part2 = () => {
@@ -86,66 +86,60 @@ export const part2 = () => {
   const dimensions: Point = { x: matrix[0].length, y: matrix.length };
   let count = 0;
 
-  for (let y = 0; y < dimensions.y; y++) {
-    for (let x = 0; x < dimensions.x; x++) {
-      const testMatrix = matrix.map((line) => line.map((item) => item));
-      // already an obstacle
-      if (testMatrix[y][x] == "#") {
-        continue;
-      }
-      // don't consider starting position
-      if (testMatrix[y][x] == "^") {
-        continue;
-      }
+  walkPath(matrix).forEach((point) => {
+    const at = findStartingPosition(matrix);
+    let curr = ORDER.find((dir) => dir.char == matrix[at.y][at.x]);
+    const pointLog: PointWithDirection[] = [];
 
-      const at = findStartingPosition(testMatrix);
-      let curr = ORDER.find((dir) => dir.char == testMatrix[at.y][at.x]);
-      const pointLog: PointWithDirection[] = [];
+    // skip the starting point
+    if (matrix[point.y][point.x] == "^") {
+      return;
+    }
 
-      testMatrix[y][x] = "#";
+    matrix[point.y][point.x] = "#";
 
-      while (
-        at.x + curr.x >= 0 &&
-        at.x + curr.x < dimensions.x &&
-        at.y + curr.y >= 0 &&
-        at.y + curr.y < dimensions.y
-      ) {
-        if (testMatrix[at.y + curr.y][at.x + curr.x] == "#") {
+    while (
+      at.x + curr.x >= 0 &&
+      at.x + curr.x < dimensions.x &&
+      at.y + curr.y >= 0 &&
+      at.y + curr.y < dimensions.y
+    ) {
+      if (matrix[at.y + curr.y][at.x + curr.x] == "#") {
+        curr =
+          ORDER[
+            (ORDER.findIndex((dir) => dir.char == curr.char) + 1) % ORDER.length
+          ];
+        // double turns are possible
+        if (matrix[at.y + curr.y][at.x + curr.x] == "#") {
           curr =
             ORDER[
               (ORDER.findIndex((dir) => dir.char == curr.char) + 1) %
                 ORDER.length
             ];
-          // double turns are possible
-          if (testMatrix[at.y + curr.y][at.x + curr.x] == "#") {
-            curr =
-              ORDER[
-                (ORDER.findIndex((dir) => dir.char == curr.char) + 1) %
-                  ORDER.length
-              ];
-          }
         }
-
-        at.x += curr.x;
-        at.y += curr.y;
-
-        // We have already been here, in this direction. Loop found
-        if (
-          pointLog.find(
-            (point) =>
-              at.x == point.x &&
-              at.y == point.y &&
-              curr.char == point.direction.char,
-          )
-        ) {
-          count++;
-          break;
-        }
-
-        pointLog.push({ ...at, direction: curr });
       }
+
+      at.x += curr.x;
+      at.y += curr.y;
+
+      // We have already been here, in this direction. Loop found
+      if (
+        pointLog.find(
+          (point) =>
+            at.x == point.x &&
+            at.y == point.y &&
+            curr.char == point.direction.char,
+        )
+      ) {
+        count++;
+        break;
+      }
+
+      pointLog.push({ ...at, direction: curr });
     }
-  }
+
+    matrix[point.y][point.x] = ".";
+  });
 
   return count;
 };
